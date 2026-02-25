0a7f2c08a7f733818d89251d885166691beeca15
ikerinsti <ikerfernandezfp@ibf.cat>
2026-02-25 16:54:38 +0100

    todo arreglado 25/2

--- Blog.php
+++ Blog.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="style.css">
 <div>
     <?php include_once ('header.php'); ?>
 </div>
--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="headerStyle.css">
 <header>
   <nav>
     <div class="logo">
