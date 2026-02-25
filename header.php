5c0d99562adc2271740df7d516e20cdcea221411
ikerinsti <ikerfernandezfp@ibf.cat>
2026-02-25 16:43:51 +0100

    Pagina contacto y correción de algunos detalles home 25/2

--- header.php
+++ header.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="HeaderStyle.css">
 </head>
 <body>
     <header>
         <nav>
           <div class="logo">
-            <a class="logo-menu" href="index.php">AK<span>Web</span>Services</a>
+            <a class="logo-menu" href="index.php"><img src="img/Logo AK_Web_Service_Vector_2.svg" alt=""></a>
           </div>
           <div class="menu-nav">
             <ul class="ul-menu">
