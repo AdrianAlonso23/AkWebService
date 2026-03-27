67ec9221058a9c60deb5d155d1308c2c9c3be141
ikerinsti <ikerfernandezfp@ibf.cat>
2026-03-27 16:48:38 +0100

    Arreglo de conflicto del merge en servicios header e iconos

--- header.php
+++ header.php
@@ -1,48 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <nav>
-          <div class="logo">
-            <a class="logo-menu" href="index.php">AK<span>Web</span>Services</a>
-          </div>
-          <div class="menu-nav">
-            <ul class="ul-menu">
-              <li class="li-menu"><a href="index.php">Inicio</a></li>
-              <li class="li-menu"><a href="Servicios.php">Servicios</a></li>
-              <li class="li-menu"><a href="Contacto.php">Contacto</a></li>
-              <li class="li-menu"><a href="Blog.php">Blog</a></li>
-            </ul>
-          </div>
-        </nav>
-    </header>
-</body>
-</html>
-=======
-=======
->>>>>>> main
 <link rel="stylesheet" href="headerStyle.css">
 <header>
   <nav>
     <div class="logo">
-<<<<<<< HEAD
       <img src="img/Logo AK_Web_Service_Vector_2.svg" alt="Logo de AK Web Services">
     </div>
     <div class="menu-nav">
-=======
-      <a class="logo-menu" href="index.php">AK<span>Web</span>Services</a>
-    </div>
-    <div class="menu-nav">
-
->>>>>>> main
       <ul class="ul-menu">
         <li class="li-menu"><a href="index.php">Inicio</a></li>
         <li class="li-menu"><a href="Servicios.php">Servicios</a></li>
@@ -52,7 +14,3 @@
     </div>
   </nav>
 </header>
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> main
